e7d6aa932a10ff824dc0d450648b9540ac8a6b3c
baywong@foxmail.com <Wangwan0130>
2017-08-03 00:38:53 +0800

    优化有参的pyhton简单的装饰器

--- PythonStu1/decorator.py
+++ PythonStu1/decorator.py
@@ -3,9 +3,9 @@ import  time
 # 简单的装饰器
 
 def timer(func):
-    def deco():
+    def deco(*args,**kwargs):
         start_time = time.time()
-        func()
+        func(*args,**kwargs)
         stop_time = time.time()
         print("运行时间%s" % (stop_time - start_time))
     return deco
@@ -15,8 +15,12 @@ def foo1():
     time.sleep(1)
     print("in the foo1")
 @timer
-def foo2():
+def foo2(name ,age):
     time.sleep(2)
-    print("in the foo2")
+    print("in the foo2 name is %s age is %s" % (name,age))
+# def foo3():
+#     time.sleep(2)
+#     print("in the foo")
+#     return "return func"
 foo1()
-foo2()
+foo2("ww","23")
